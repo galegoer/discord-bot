d5c69ccfc088aa692e2da65c71dc227c631ef849
galegoer <eric.m.galego@gmail.com>
2023-06-19 23:01:24 -0400

    fixed code for random picker

--- src/commands/misc/random-picker.js
+++ src/commands/misc/random-picker.js
@@ -5,7 +5,7 @@ const GIFEncoder = require('gifencoder');
 const fs = require('fs');
 
 module.exports = {
-    name: 'Random Picker',
+    name: 'random-picker',
     description: 'Picks a random choice from the list of names provided.',
     testOnly: true,
     options: [
@@ -27,6 +27,10 @@ module.exports = {
         const num_entries = entryList.length;
         const canvas = createCanvas(WIDTH, HEIGHT) // set the height and width of the canvas
         const ctx = canvas.getContext('2d');
+        // TODO: if we use exact it won't work in for loop for %, if we use ceil it'll be a bit off but will work
+        const slice = Math.ceil(WIDTH / num_entries); // this is the width of each rectangle
+        let winnerInd = Math.floor(Math.random() * num_entries);
+        let winnerName = entryList[winnerInd];
 
         ctx.textAlign = 'center';
         ctx.font = '22px serif';
@@ -34,36 +38,39 @@ module.exports = {
             ctx.fillStyle = '#b9b6b5';
             ctx.fillRect(0, 0, WIDTH, HEIGHT); // fill the entire canvas
             ctx.fillStyle = 'black';
-            let xposition = 50;
+            let xposition = slice/2;
            
-            for(let i=1; i<=num_entries; i++) {
-                ctx.fillText(entries[i], xposition, HEIGHT/2);
-                xposition += WIDTH/num_entries;
+            for(let i=0; i<num_entries; i++) {
+                // 22 based on pixel size of font
+                if (entryList[i].length * 22 > slice ) {
+                    entryList[i] = entryList[i].substring(0, Math.floor(slice/22)) + '...'
+                }
+                ctx.fillText(entryList[i], xposition, HEIGHT/2);
+                xposition += slice;
             }
         };
 
-        
         const encoder = new GIFEncoder(WIDTH, HEIGHT);
         let delay = 1;
         encoder.start(); // starts the encoder
         
-        const slice = WIDTH / num_entries; // this is the width of each rectangle// frame 1
         background();
         
-        let iterations = (Math.floor(Math.random() * num_entries) + 1) + 30; // 1 to 10 (3 iterations) smaller numbers will be more iterations but still fine
-        console.log(iterations);
-        let optionNum = 1;
-        for (let i=0; i<iterations * slice; i += slice) {
+        let iterations = winnerInd + (num_entries * 4);
+        console.log(winnerInd, iterations);
+        let optionNum = 0;
+        for (let i=0; i<=iterations * slice; i += slice) {
             ctx.fillStyle = '#72ec5c';
             ctx.fillRect(i%WIDTH, 0, slice, HEIGHT);
             ctx.fillStyle = 'black';
             // Replace text so option is still visible
-            ctx.fillText(entries[i], i%WIDTH+50, HEIGHT/2);
+            ctx.fillText(entryList[optionNum], (i%WIDTH)+slice/2, HEIGHT/2);
             encoder.addFrame(ctx);
             background();
             encoder.setDelay(delay);
-            optionNum = optionNum >= 10 ? 1 : optionNum + 1;
-            delay += delay;
+            optionNum = optionNum === num_entries - 1 ? 0 : optionNum + 1;
+
+            delay += 10;
         }
         
         // end the encoding
@@ -71,10 +78,9 @@ module.exports = {
 
         const buffer = encoder.out.getData();
 
-        // TODO: change to output w/o having to save locally
-
-        fs.writeFile('example.gif', buffer, error => {
-            error ? console.log(error) : null;
+        interaction.reply({content: `The winner is... || ${entryList[winnerName]} ||`, files: [{attachment: buffer, name: 'temp.gif'}]})
+        .catch((error) => {
+            console.error('Error sending file: ', error);
         });
     },
 };
